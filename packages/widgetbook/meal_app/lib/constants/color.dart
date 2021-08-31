84326349c1e2952b6038bb066891fa2e6dfbe93d
Abdelrahman Mostafa Elmarakby <43115402+abdelrahmanelmarakby@users.noreply.github.com>
2021-08-31 10:00:19 +0200

    Changed variables to const
    -changed variables to constant with const

--- packages/widgetbook/meal_app/lib/constants/color.dart
+++ packages/widgetbook/meal_app/lib/constants/color.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ColorConstants {
-  static Color backgroundColorDark = Color(0xFF1E2026);
-  static Color backgroundColorLight = Color(0xFFECECEC);
-  static Color cardColor = Color(0xFF2A2C31);
-  static Color primaryColor = Color(0xFF7C40FF);
-  static Color lightFontColor = Color(0xFF1E2026);
+  static const Color backgroundColorDark = Color(0xFF1E2026);
+  static const Color backgroundColorLight = Color(0xFFECECEC);
+  static const Color cardColor = Color(0xFF2A2C31);
+  static const Color primaryColor = Color(0xFF7C40FF);
+  static const Color lightFontColor = Color(0xFF1E2026);
 }
