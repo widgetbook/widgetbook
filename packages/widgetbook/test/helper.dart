f8dfb46de98d820fd0e2c363e1ace057a560fc47
Jens Horstmann <jens@firecrown.pro>
2021-10-01 07:28:18 +0200

    recommit

--- packages/widgetbook/test/helper.dart
+++ packages/widgetbook/test/helper.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension WidgetTesterExtension on WidgetTester {
-  Future<void> pumpWidgetWithMaterialApp(Widget widget) async {
+  Future<void> pumpWidgetWithMaterialApp(
+    Widget widget,
+  ) async {
     return this.pumpWidget(
       MaterialApp(
         home: widget,
@@ -10,14 +12,18 @@ extension WidgetTesterExtension on WidgetTester {
     );
   }
 
-  Future<T> invokeAndPumpProvider<T>(VoidCallback function) async {
-    function();
+  Future<T> invokeMethodAndReturnPumpedProvider<T>(
+    VoidCallback method,
+  ) async {
+    method();
     await this.pump();
     return getProvider<T>(this);
   }
 }
 
-T getProvider<T>(WidgetTester tester) {
+T getProvider<T>(
+  WidgetTester tester,
+) {
   var providerFinder = find.byType(T);
   return tester.firstWidget(providerFinder) as T;
 }
