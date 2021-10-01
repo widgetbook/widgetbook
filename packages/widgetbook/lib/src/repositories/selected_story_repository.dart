46a99ced60c60767d774cdb6dff62108f4b3e855
Jens Horstmann <jens@firecrown.pro>
2021-10-01 14:29:35 +0200

    renamed SingleValueRepository to ValueRepository

--- packages/widgetbook/lib/src/repositories/selected_story_repository.dart
+++ packages/widgetbook/lib/src/repositories/selected_story_repository.dart
@@ -1,4 +1,4 @@
-import 'package:widgetbook/src/repositories/single_value_repository.dart';
+import 'package:widgetbook/src/repositories/value_repository.dart';
 import 'package:widgetbook/widgetbook.dart';
 
-class SelectedStoryRepository extends SingleValueRepository<Story> {}
+class SelectedStoryRepository extends ValueRepository<Story> {}
