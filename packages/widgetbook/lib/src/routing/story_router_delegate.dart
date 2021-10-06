34f2a32474ff44bdfe9abb4166ecc81011de807b
Jens Horstmann <jens@firecrown.pro>
2021-10-06 12:21:52 +0200

    added NoAnimationTransitionDelegate

--- packages/widgetbook/lib/src/routing/story_router_delegate.dart
+++ packages/widgetbook/lib/src/routing/story_router_delegate.dart
@@ -9,6 +9,41 @@ import 'package:widgetbook/src/widgets/wrapper.dart';
 
 import '../../widgetbook.dart';
 
+class NoAnimationTransitionDelegate extends TransitionDelegate<void> {
+  @override
+  Iterable<RouteTransitionRecord> resolve({
+    required List<RouteTransitionRecord> newPageRouteHistory,
+    required Map<RouteTransitionRecord?, RouteTransitionRecord>
+        locationToExitingPageRoute,
+    required Map<RouteTransitionRecord?, List<RouteTransitionRecord>>
+        pageRouteToPagelessRoutes,
+  }) {
+    final List<RouteTransitionRecord> results = <RouteTransitionRecord>[];
+
+    for (final RouteTransitionRecord pageRoute in newPageRouteHistory) {
+      if (pageRoute.isWaitingForEnteringDecision) {
+        pageRoute.markForAdd();
+      }
+      results.add(pageRoute);
+    }
+    for (final RouteTransitionRecord exitingPageRoute
+        in locationToExitingPageRoute.values) {
+      if (exitingPageRoute.isWaitingForExitingDecision) {
+        exitingPageRoute.markForRemove();
+        final List<RouteTransitionRecord>? pagelessRoutes =
+            pageRouteToPagelessRoutes[exitingPageRoute];
+        if (pagelessRoutes != null) {
+          for (final RouteTransitionRecord pagelessRoute in pagelessRoutes) {
+            pagelessRoute.markForRemove();
+          }
+        }
+      }
+      results.add(exitingPageRoute);
+    }
+    return results;
+  }
+}
+
 class StoryRouterDelegate extends RouterDelegate<StoryRoutePath>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<StoryRoutePath> {
   @override
@@ -25,6 +60,7 @@ class StoryRouterDelegate extends RouterDelegate<StoryRoutePath>
   @override
   Widget build(BuildContext context) {
     return Navigator(
+      transitionDelegate: NoAnimationTransitionDelegate(),
       pages: [
         MaterialPage(
           key: ValueKey(currentConfiguration.path),
