440395bf17d906661019ff4e9a7c066cbcd2da97
Jens Horstmann <jens@firecrown.pro>
2021-10-06 12:28:58 +0200

    removed discord button, removed dependency

--- packages/widgetbook/lib/src/widgets/controls_bar.dart
+++ packages/widgetbook/lib/src/widgets/controls_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:widgetbook/src/constants/constants.dart';
 import 'package:widgetbook/src/widgets/zoom_handle.dart';
-import 'package:widgetbook/src/widgets/brand_handle.dart';
 import 'package:widgetbook/src/widgets/device_bar.dart';
 import 'package:widgetbook/src/widgets/theme_handle.dart';
 
@@ -13,34 +12,32 @@ class ControlsBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: Constants.controlBarHeight,
-      child: Row(children: [
-        const SizedBox(
-          width: 16,
-        ),
-        Expanded(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                ZoomHandle(),
-                SizedBox(
-                  width: 40,
-                ),
-                ThemeHandle(),
-                SizedBox(
-                  width: 40,
-                ),
-                DeviceBar(),
-              ],
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  ZoomHandle(),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  ThemeHandle(),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  DeviceBar(),
+                ],
+              ),
             ),
           ),
-        ),
-        const BrandHandle(),
-        const SizedBox(
-          width: 16,
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
