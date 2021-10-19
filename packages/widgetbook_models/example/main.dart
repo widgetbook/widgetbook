6339f7fd21438969b79f4c9cf6fd44f4dbfaadee
Jens Horstmann <jens@firecrown.pro>
2021-10-19 11:32:29 +0200

    added example

---
+++ packages/widgetbook_models/example/main.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: type_annotate_public_apis
+
+import 'package:widgetbook_models/widgetbook_models.dart';
+
+Device iPhone = Apple.iPhone12;
+Device s10 = Samsung.s10;
+Device customDevice = const Device.special(
+  name: 'Squared device',
+  resolution: Resolution(
+    nativeSize: DeviceSize(
+      height: 200,
+      width: 200,
+    ),
+    scaleFactor: 2,
+  ),
+);
