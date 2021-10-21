387b3fd97a2de2b8629901c641c1ca40d64ef0e7
Jens Horstmann <jens@firecrown.pro>
2021-10-21 10:14:31 +0200

    added export of widgetbook_models to widgetbook_annotation

--- packages/widgetbook_annotation/lib/widgetbook_annotation.dart
+++ packages/widgetbook_annotation/lib/widgetbook_annotation.dart
@@ -1,5 +1,7 @@
 library widgetbook_annotation;
 
+export 'package:widgetbook_models/widgetbook_models.dart';
+
 export 'src/widgetbook_app.dart';
 export 'src/widgetbook_story.dart';
 export 'src/widgetbook_theme.dart';
