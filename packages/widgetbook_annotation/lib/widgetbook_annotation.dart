bd8c4fe06807dbef6e22750edec5bf77b2ffb3fe
Jens Horstmann <jens@firecrown.pro>
2021-11-03 12:27:11 +0100

    changed WidgetbookStory to WidgetbookUseCase

--- packages/widgetbook_annotation/lib/widgetbook_annotation.dart
+++ packages/widgetbook_annotation/lib/widgetbook_annotation.dart
@@ -3,5 +3,5 @@ library widgetbook_annotation;
 export 'package:widgetbook_models/widgetbook_models.dart';
 
 export 'src/widgetbook_app.dart';
-export 'src/widgetbook_story.dart';
 export 'src/widgetbook_theme.dart';
+export 'src/widgetbook_use_case.dart';
