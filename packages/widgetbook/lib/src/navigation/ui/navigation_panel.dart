bcf2da8dced2e7f3a329f1cfefa701a1dcf86561
Jens Horstmann <jens@firecrown.pro>
2022-01-31 15:27:01 +0100

    added icons

--- packages/widgetbook/lib/src/navigation/ui/navigation_panel.dart
+++ packages/widgetbook/lib/src/navigation/ui/navigation_panel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:widgetbook/src/constants/radii.dart';
 import 'package:widgetbook/src/models/app_info.dart';
 import 'package:widgetbook/src/models/organizers/organizers.dart';
+import 'package:widgetbook/src/widgets/expanders/expander_row.dart';
 import 'package:widgetbook/src/widgets/header.dart';
 import 'package:widgetbook/src/widgets/search_bar.dart';
 import 'package:widgetbook/src/widgets/tiles/category_tile.dart';
