86df40ae872ab47279f7246c962bf2743de47cb6
Jens Horstmann <jens@firecrown.pro>
2022-02-01 13:56:13 +0100

    added documentation for ZoomProvider and ToolProvider

--- packages/widgetbook/lib/src/mouse_tool/tool_provider.dart
+++ packages/widgetbook/lib/src/mouse_tool/tool_provider.dart
@@ -1,6 +1,11 @@
 import 'package:widgetbook/src/mouse_tool/tool_state.dart';
 import 'package:widgetbook/src/state_change_notifier.dart';
 
+/// [ToolProvider] defines a bunch of mouse related tools.
+///
+/// For instance:
+/// - Mouse for selecting and clicking
+/// - Mouse for moving and zooming a previewed device.
 class ToolProvider extends StateChangeNotifier<ToolState> {
   ToolProvider({
     ToolState? state,
@@ -8,10 +13,15 @@ class ToolProvider extends StateChangeNotifier<ToolState> {
           state: state ?? ToolState(),
         );
 
+  /// Activates the mode for moving and zooming a device
   void moveTool() {
     state = ToolState(mode: SelectionMode.move);
   }
 
+  /// Activates the mode for interacting with a widget and device
+  ///
+  /// This mode is required for scrolling within a widget that contains e.g. a
+  /// ScrollViewer.
   void selecionTool() {
     state = ToolState(mode: SelectionMode.normal);
   }
