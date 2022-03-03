d624596804809f9a3cc8c12004f4d0d641ece422
Joshua Petitma <joshua@petit.dev>
2022-03-03 08:28:10 -0500

    Create nullable checkbox

---
+++ packages/widgetbook/lib/src/knobs/nullable_checkbox.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:widgetbook/src/knobs/knobs.dart';
+
+class NullableCheckbox<T> extends StatelessWidget {
+  const NullableCheckbox({
+    Key? key,
+    required this.cachedValue,
+    required this.value,
+    required this.label,
+  }) : super(key: key);
+
+  final String label;
+  final T cachedValue;
+  final T? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: cachedValue == null,
+      onChanged: (v) {
+        T? newValue;
+        switch (v) {
+          case null:
+          case true:
+            newValue = null;
+            break;
+          case false:
+            newValue = cachedValue;
+        }
+        context.read<KnobsNotifier>().update(label, newValue);
+      },
+    );
+  }
+}
