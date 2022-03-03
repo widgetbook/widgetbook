0c85ab36041d77c68cd7657b433c7f95f68e7d67
Joshua Petitma <joshua@petit.dev>
2022-03-03 06:31:38 -0500

    Create test for text knob

---
+++ packages/widgetbook/test/src/knobs/text_knob_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widgetbook/src/knobs/knobs.dart';
+import 'package:widgetbook/src/knobs/text_knob.dart';
+
+import '../../helper/widget_test_helper.dart';
+import 'knobs_test.dart';
+
+void main() {
+  testWidgets(
+    'Equality operator works correctly',
+    (WidgetTester tester) async {
+      final first = TextKnob(label: 'first', value: 'hello');
+      final second = TextKnob(label: 'second', value: 'goodbye');
+
+      expect(first, equals(TextKnob(label: 'first', value: 'hello')));
+      expect(first, isNot(equals(second)));
+    },
+  );
+
+  testWidgets(
+    'Text knob initial value works',
+    (WidgetTester tester) async {
+      await tester.pumpWidgetWithMaterialApp(
+        renderWithKnobs(
+            build: (context) => [
+                  Text(context.knobs.text(
+                    label: 'label',
+                    initialValue: 'Hi',
+                  ))
+                ]),
+      );
+      expect(find.text('Hi'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Text knob functions',
+    (WidgetTester tester) async {
+      await tester.pumpWidgetWithMaterialApp(
+        renderWithKnobs(
+            build: (context) => [
+                  Text(context.knobs.text(
+                    label: 'label',
+                  ))
+                ]),
+      );
+      await tester.enterText(find.byKey(const Key('label-textKnob')), 'Bye');
+      await tester.pumpAndSettle();
+
+      expect(find.text('Bye'), findsOneWidget);
+    },
+  );
+}
