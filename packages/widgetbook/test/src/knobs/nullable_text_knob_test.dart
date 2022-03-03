db657fdf3ef3bad7864dcc41ef04347e5aceded4
Joshua Petitma <joshua@petit.dev>
2022-03-03 08:28:01 -0500

    Create test for nullable text

---
+++ packages/widgetbook/test/src/knobs/nullable_text_knob_test.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widgetbook/src/knobs/knobs.dart';
+import 'package:widgetbook/src/knobs/nullable_string_knob.dart';
+
+import '../../helper/widget_test_helper.dart';
+import 'knobs_test.dart';
+
+void main() {
+  testWidgets(
+    'Equality operator works correctly',
+    (WidgetTester tester) async {
+      final first = NullableStringKnob(label: 'first', value: null);
+      final second = NullableStringKnob(label: 'second', value: 'value');
+      expect(first, equals(NullableStringKnob(label: 'first', value: null)));
+      expect(first, isNot(equals(second)));
+    },
+  );
+
+  testWidgets(
+    'Nullable String knob functions',
+    (WidgetTester tester) async {
+      await tester.pumpWidgetWithMaterialApp(
+        renderWithKnobs(build: (context) {
+          final value = context.knobs.nullableString(
+            label: 'label',
+            initialValue: null,
+          );
+
+          final String text = value ?? 'default';
+
+          return [Text(text)];
+        }),
+      );
+
+      expect(find.text('default'), findsOneWidget);
+
+      await tester.tap(find.byKey(const Key('label-nullableTextKnob-check')));
+      await tester.pumpAndSettle();
+      await tester.enterText(
+          find.byKey(const Key('label-nullableTextKnob-check')), 'awesome');
+      await tester.pumpAndSettle();
+      expect(find.text('awesome'), findsWidgets);
+      expect(find.text('default'), findsNothing);
+    },
+  );
+
+  testWidgets(
+    'Nullable String remembers previosu value',
+    (WidgetTester tester) async {
+      await tester.pumpWidgetWithMaterialApp(
+        renderWithKnobs(build: (context) {
+          final value = context.knobs.nullableString(
+            label: 'label',
+            initialValue: null,
+          );
+
+          final String text = value ?? 'default';
+
+          return [Text(text)];
+        }),
+      );
+
+      expect(find.text('default'), findsOneWidget);
+
+      await tester.tap(find.byKey(const Key('label-nullableTextKnob-check')));
+      await tester.pumpAndSettle();
+      await tester.enterText(
+          find.byKey(const Key('label-nullableTextKnob-check')), 'awesome');
+      await tester.pumpAndSettle();
+      expect(find.text('awesome'), findsWidgets);
+      expect(find.text('default'), findsNothing);
+
+      await tester.tap(find.byKey(const Key('label-nullableTextKnob-check')));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('label-nullableTextKnob-check')));
+      await tester.pumpAndSettle();
+      expect(find.text('awesome'), findsWidgets);
+    },
+  );
+}
