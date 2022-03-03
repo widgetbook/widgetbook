941c5db6c5ee122a0428683a57c77c9ca8e16386
Joshua Petitma <joshua@petit.dev>
2022-03-03 08:28:23 -0500

    Use nullable checkbox in nullable text

---
+++ packages/widgetbook/lib/src/knobs/nullable_text.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:widgetbook/src/knobs/knobs.dart';
+import 'package:widgetbook/src/knobs/nullable_checkbox.dart';
+
+class NullableTextKnob extends Knob<String?> {
+  NullableTextKnob({
+    required String label,
+    String? description,
+    required String? value,
+  }) : super(
+          label: label,
+          description: description,
+          value: value,
+        );
+
+  @override
+  Widget build() => NullableTexteanKnobWidget(
+        label: label,
+        description: description,
+        value: value,
+      );
+}
+
+class NullableTexteanKnobWidget extends StatefulWidget {
+  const NullableTexteanKnobWidget({
+    Key? key,
+    required this.label,
+    required this.description,
+    required this.value,
+  }) : super(key: key);
+
+  final String label;
+  final String? description;
+  final String? value;
+
+  @override
+  State<NullableTexteanKnobWidget> createState() =>
+      _NullableTexteanKnobWidgetState();
+}
+
+class _NullableTexteanKnobWidgetState extends State<NullableTexteanKnobWidget> {
+  String value = '';
+  final controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.value != null) {
+      value = widget.value!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Center(
+          child: Text(
+            '${widget.label}:',
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextField(
+            key: Key('${widget.label}-textKnob'),
+            controller: controller,
+            onChanged: (v) {
+              context.read<KnobsNotifier>().update(widget.label, v);
+            },
+          ),
+        ),
+        NullableCheckbox(
+          cachedValue: value,
+          value: widget.value,
+          label: widget.label,
+        )
+      ],
+    );
+  }
+}
