70c8dd72ca7eef8c197013b30e5d9428a15958f7
Jens Horstmann <jens@widgetbook.io>
2022-05-10 19:43:27 +0200

    renamed device_frame.dart to widgetbook_device_frame.dart

--- packages/widgetbook/lib/widgetbook.dart
+++ packages/widgetbook/lib/widgetbook.dart
@@ -4,5 +4,6 @@ export 'package:widgetbook/src/models/models.dart';
 export 'package:widgetbook/src/rendering/builders/builders.dart';
 export 'package:widgetbook/src/theming/widgetbook_theme.dart';
 export 'package:widgetbook/src/widgetbook.dart';
-export 'package:widgetbook_device_frame/device_frame.dart' hide DeviceType;
+export 'package:widgetbook_device_frame/widgetbook_device_frame.dart'
+    hide DeviceType;
 export 'package:widgetbook_models/widgetbook_models.dart';
