bcc230ae86c89d9b40e10432661fb50ab7335392
Jens Horstmann <jens@widgetbook.io>
2022-10-26 14:16:53 +0200

    added more context to the implementation

--- packages/widgetbook_cli/bin/commands/analyze.dart
+++ packages/widgetbook_cli/bin/commands/analyze.dart
@@ -1,6 +1,7 @@
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
 
@@ -10,13 +11,62 @@ class WidgetbookLocalesVisitor extends RecursiveAstVisitor<void> {
   @override
   void visitAnnotation(Annotation node) {
     node.visitChildren(this);
+
     print(node.name);
     if (node.name.name == 'WidgetbookLocales') {
-      print('It is WidgetbookLocales');
+      final locales = <String>[];
+      // This is some super sketchy implementation
+      final declarationNode = node.parent;
+      final variableDeclarationNode =
+          declarationNode.childEntities.toList()[1] as AstNode;
+      final xxx = variableDeclarationNode.childEntities.toList()[1] as AstNode;
+      // From here on the code is pretty much the same as in the LocalesResolver
+      // from the widgetbook_generator package
+      // see:
+      // packages/widgetbook_generator/lib/resolvers/locales_resolver.dart
+      final entities = xxx.childEntities.toList();
+      if (entities[0] is Token &&
+          entities[1] is Token &&
+          entities[2] is ListLiteral) {
+        final listLiteral = entities[2] as ListLiteral;
+
+        // TODO this is likely the most flaky code that has ever been written
+        // check the following cases
+        // - Locale('en')
+        // - const Locale('en')
+        // - Locale('en', 'US')
+        // - const Locale('en', 'US')
+        // - additional methods, e.g. Locale.fromString('...') and similar stuff
+        // Maybe it's best to just use a regex
+        final localeDefinitions = listLiteral.elements.toList();
+        for (final localeDefinition in localeDefinitions) {
+          // If expression starts WITHOUT const e.g. Local('en'), this will
+          // be called
+          if (localeDefinition is MethodInvocation) {
+            final stringLiteral =
+                localeDefinition.argumentList.arguments.first as StringLiteral;
+            final value = stringLiteral.stringValue!;
+            locales.add(value);
+          }
+          // If expression starts WITH const e.g. const Local('en'), this will
+          // be called
+          if (localeDefinition is InstanceCreationExpression) {
+            final stringLiteral =
+                localeDefinition.argumentList.arguments.first as StringLiteral;
+            final value = stringLiteral.stringValue!;
+            locales.add(value);
+          }
+        }
+      }
+
+      print(locales);
     }
+
     return;
   }
 }
+
+class TextException implements Exception {}
 
 class AnalyzeCommand extends WidgetbookCommand {
   AnalyzeCommand({
@@ -49,14 +99,15 @@ class AnalyzeCommand extends WidgetbookCommand {
           !file.isPart &&
           file.isLibrary &&
           filePath.endsWith('.dart')) {
-        final result = collection
+        final someResolvedResult = await collection
             .contextFor(
               filePath,
             )
             .currentSession
-            .getParsedUnit(
+            .getResolvedUnit(
               filePath,
-            ) as ParsedUnitResult;
+            );
+        final result = someResolvedResult as ResolvedUnitResult;
 
         result.unit.visitChildren(WidgetbookLocalesVisitor());
       }
