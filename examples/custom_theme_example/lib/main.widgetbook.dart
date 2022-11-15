2849dceead1d6d7b03a2d3b4c228502ddcc75517
Eric Muli <erickiokom@students.uonbi.ac.ke>
2022-11-15 17:25:37 +0300

    making some progress

--- examples/custom_theme_example/lib/main.widgetbook.dart
+++ examples/custom_theme_example/lib/main.widgetbook.dart
@@ -3,16 +3,14 @@ import 'package:custom_theme_example/awesome_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgetbook/widgetbook.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart' as anno;
 
 void main() {
   runApp(const HotReload());
 }
 
-@anno.WidgetbookTheme(name: 'Default')
+// @anno.WidgetbookTheme(name: 'Default')
 AppThemeData themeData = AppThemeData(
   color: Colors.yellow,
-  // typography: TypographyData(123),
 );
 
 class HotReload extends StatelessWidget {
@@ -20,7 +18,6 @@ class HotReload extends StatelessWidget {
 
   GoRouter getRouter(Widget child) {
     return GoRouter(
-      
       routes: [
         GoRoute(
           path: '/',
@@ -38,54 +35,59 @@ class HotReload extends StatelessWidget {
     );
 
     return Widgetbook(
-      addons: [
-        CustomThemeAddon<AppThemeData>(
-          themeSetting: CustomThemeSetting(
-            themes: [
-              widgetbookTheme,
-            ],
-            activeThemes: {
-              widgetbookTheme,
-            },
-          ),
-        ),
-      ],
-      appInfo: AppInfo(name: 'Custom Theme Example'),
-      categories: [
-        WidgetbookCategory(
-          name: 'Default',
-          widgets: [
-            WidgetbookComponent(
-              name: 'Awesome Widget',
-              useCases: [
-                WidgetbookUseCase(
-                  name: 'Default',
-                  builder: (context) => const AwesomeWidget(),
-                )
+        addons: [
+          CustomThemeAddon<AppThemeData>(
+            themeSetting: CustomThemeSetting(
+              themes: [
+                widgetbookTheme,
               ],
+              activeThemes: {
+                widgetbookTheme,
+              },
             ),
-          ],
-        )
-      ],
-      appBuilder: (context, child) {
-       
-        final router = getRouter(child);
-        // This is actually important
-        final frameBuilder = context.frameBuilder;
-        return frameBuilder.builder(
-            context,
-            AppTheme(
-                data: context.theme(),
-                child: WidgetsApp.router(
-                  color: context.theme<AppThemeData>().color,
-                  builder: (context, childWidget) {
-                    return childWidget ?? child;
-                  },
-                  debugShowCheckedModeBanner: false,
-                  routeInformationParser: router.routeInformationParser,
-                  routerDelegate: router.routerDelegate,
-                )));
-      },
-    );
+          ),
+        ],
+        appInfo: AppInfo(name: 'Custom Theme Example'),
+        categories: [
+          WidgetbookCategory(
+            name: 'Default',
+            widgets: [
+              WidgetbookComponent(
+                name: 'Awesome Widget',
+                useCases: [
+                  WidgetbookUseCase(
+                    name: 'Default',
+                    builder: (context) => const AwesomeWidget(),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+        //THis did work but the theme is not injected in the widget tree
+        // still  looking into it
+        appBuilder: (context, child) => child
+
+        //This Didn't work
+        //We should investigate why.
+
+        //   final router = getRouter(child);
+        //   // This is actually important
+        //   final frameBuilder = context.frameBuilder;
+        //   return frameBuilder.builder(
+        //       context,
+        //       AppTheme(
+        //           data: context.theme(),
+        //           child: WidgetsApp.router(
+        //             color: context.theme<AppThemeData>().color,
+        //             builder: (context, childWidget) {
+        //               return childWidget ?? child;
+        //             },
+        //             debugShowCheckedModeBanner: false,
+        //             routeInformationParser: router.routeInformationParser,
+        //             routerDelegate: router.routerDelegate,
+        //           )));
+        // },
+        );
   }
 }
