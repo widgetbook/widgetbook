7ee29901bae3204cce8b95efc476c9f6866af6b2
muli_eriq <erickiokom@students.uonbi.ac.ke>
2022-12-02 15:56:57 +0300

    renderging second theme only

--- packages/widgetbook/lib/src/addons/theme_addon/theme_selection_provider.dart
+++ packages/widgetbook/lib/src/addons/theme_addon/theme_selection_provider.dart
@@ -8,24 +8,28 @@ class ThemeSettingProvider<T> extends ValueNotifier<ThemeSetting<T>> {
 
   void tapped(WidgetbookTheme<T> theme) {
     final currentSelection = List<WidgetbookTheme<T>>.filled(
-        value.themes.length, value.activeThemes.first,
-        growable: true);
+      value.themes.length,
+      value.activeThemes.first,
+      growable: true,
+    );
 
     log(currentSelection.length.toString());
-    currentSelection
-      ..add(value.activeThemes.map((e) => e).toList().elementAt(1))
-      ..toSet();
+
+    currentSelection[0] = value.activeThemes.first;
 
     // ..addAll(value.activeTextScales.map((e) => e));
     log('themes length ${value.activeThemes.length}');
+    log('selected themes length ${theme.name}');
     log('current selection length ${currentSelection.length}');
     if (currentSelection.contains(theme)) {
       currentSelection.remove(theme);
     } else {
+      currentSelection[value.themes.indexOf(theme)] = theme;
+      log('index of ${theme.name} ${value.themes.indexOf(theme)}');
       // currentSelection.add(theme);
-      currentSelection.isNotEmpty
-          ? currentSelection.toList().insert(value.themes.indexOf(theme), theme)
-          : currentSelection.add(theme);
+      // currentSelection.isNotEmpty
+      //     ? currentSelection.toList().insert(value.themes.indexOf(theme), theme)
+      //     : currentSelection.add(theme);
     }
 
     value = value.copyWith(activeThemes: currentSelection.toSet());
