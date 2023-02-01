de38951dfe3dbff009a889071ef4c605d4b5ef1b
Jens Horstmann <jens@widgetbook.io>
2023-02-01 12:39:07 +0100

    refactor: :recycle: remove example apps

--- examples/widgetbook_annotation_example/android/app/src/main/kotlin/com/example/meal_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.meal_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/widgetbook_annotation_example/lib/blocs/meal/meal_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'meal_bloc.dart';
-
-class MealState {
-  final List<Meal> meals;
-
-  MealState({
-    this.meals = const <Meal>[],
-  });
-}
