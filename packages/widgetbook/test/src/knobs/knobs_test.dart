cda1a95357bd8533bf5c3c62b781f971944f06eb
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-05-16 16:12:48 +0300

    remove unused imports

--- packages/widgetbook/test/src/knobs/knobs_test.dart
+++ packages/widgetbook/test/src/knobs/knobs_test.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 import 'package:widgetbook/src/knobs/bool_knob.dart';
 import 'package:widgetbook/src/knobs/knob.dart';
 import 'package:widgetbook/widgetbook.dart';
-import 'package:widgetbook_core/widgetbook_core.dart' as core;
 
-import '../../helper/widget_test_helper.dart';
 import 'knob_helper.dart';
 
 void main() {
