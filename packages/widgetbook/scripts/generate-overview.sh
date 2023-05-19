63314cf4d59537e8034547118e7b3a8f64593c58
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-05-19 13:34:55 +0300

    remove scripts

--- packages/widgetbook/scripts/generate-overview.sh
+++
@@ -1,7 +0,0 @@
-# Running this file requires lcov
-# install lcov like this
-# MacOS: `brew install lcov`
-flutter test --coverage
-lcov --remove coverage/lcov.info 'lib/constants/*' -o coverage/lcov.info
-# lcov --remove coverage/lcov.info 'lib/models/*' -o coverage/lcov.info
-genhtml coverage/lcov.info -o coverage/html
