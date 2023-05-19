e23a928e1bc0646c24fba7674b2d7aab6e8de3bd
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-05-19 11:28:17 +0300

    use relative exports

--- packages/widgetbook/lib/widgetbook.dart
+++ packages/widgetbook/lib/widgetbook.dart
@@ -1,5 +1,5 @@
-export 'package:widgetbook/src/addons/addons.dart';
-export 'package:widgetbook/src/knobs/knobs_notifier.dart';
-export 'package:widgetbook/src/navigation/navigation.dart';
-export 'package:widgetbook/src/theming/widgetbook_theme.dart';
-export 'package:widgetbook/src/widgetbook.dart';
+export 'src/addons/addons.dart';
+export 'src/knobs/knobs_notifier.dart';
+export 'src/navigation/navigation.dart';
+export 'src/theming/widgetbook_theme.dart';
+export 'src/widgetbook.dart';
