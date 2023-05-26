074c1df1175b5bd0437c7b5bd3e115fc7db6206b
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-05-26 15:42:24 +0300

    add integrations docs

--- packages/widgetbook/lib/src/integrations/widgetbook_cloud_integration/web_messaging.dart
+++ packages/widgetbook/lib/src/integrations/widgetbook_cloud_integration/web_messaging.dart
@@ -1,7 +1,9 @@
 import 'dart:html';
 
 import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
 
+@internal
 void sendMessage(Map<String, dynamic> json) {
   if (!kIsWeb) return;
   window.parent?.postMessage(json, '*');
