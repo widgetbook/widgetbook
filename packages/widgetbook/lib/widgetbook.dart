5b3914fdf8e5b73eabe1cf3cc3d58ab90cd89411
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-05-26 14:20:53 +0300

    add library doc

--- packages/widgetbook/lib/widgetbook.dart
+++ packages/widgetbook/lib/widgetbook.dart
@@ -1,3 +1,7 @@
+/// A Flutter storybook that helps professionals and teams
+/// to catalogue their widgets.
+library widgetbook;
+
 export 'src/addons/addons.dart';
 export 'src/integrations/integrations.dart';
 export 'src/knobs/knobs.dart';
