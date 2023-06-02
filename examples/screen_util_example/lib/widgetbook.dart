e42c643e66f88c75bcc31cf5adbeb6f1db7b413f
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-06-03 00:21:56 +0300

    chore: fix unreferenced dependencies

--- examples/screen_util_example/lib/widgetbook.dart
+++ examples/screen_util_example/lib/widgetbook.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-import 'package:widgetbook_core/widgetbook_core.dart';
 
 import 'widgetbook.g.dart';
 
@@ -19,29 +18,8 @@ class WidgetbookApp extends StatelessWidget {
     return Widgetbook.material(
       directories: directories,
       addons: [
-        MaterialThemeAddon(
-          themes: [
-            WidgetbookTheme(
-              name: 'Light',
-              data: Themes.light,
-            ),
-            WidgetbookTheme(
-              name: 'Dark',
-              data: Themes.dark,
-            ),
-          ],
-        ),
         TextScaleAddon(
           scales: [1.0, 2.0],
-        ),
-        LocalizationAddon(
-          locales: [
-            const Locale('en', 'US'),
-          ],
-          localizationsDelegates: [
-            DefaultWidgetsLocalizations.delegate,
-            DefaultMaterialLocalizations.delegate,
-          ],
         ),
         DeviceFrameAddon(
           devices: [
