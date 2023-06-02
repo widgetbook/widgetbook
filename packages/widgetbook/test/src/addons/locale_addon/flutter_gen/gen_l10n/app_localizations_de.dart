de3cad95df43e6716b374ff7e4f41c53e7239d70
Youssef Raafat <youssefraafatnasry@gmail.com>
2023-06-02 14:40:47 +0300

    test: remove `flutter_localizations` (#709)

--- packages/widgetbook/test/src/addons/locale_addon/flutter_gen/gen_l10n/app_localizations_de.dart
+++
@@ -1,9 +0,0 @@
-import 'app_localizations.dart';
-
-/// The translations for German (`de`).
-class AppLocalizationsDe extends AppLocalizations {
-  AppLocalizationsDe([super.locale = 'de']);
-
-  @override
-  String get newTag => 'neu';
-}
--- packages/widgetbook/test/src/addons/locale_addon/flutter_gen/gen_l10n/app_localizations_fr.dart
+++
@@ -1,9 +0,0 @@
-import 'app_localizations.dart';
-
-/// The translations for French (`fr`).
-class AppLocalizationsFr extends AppLocalizations {
-  AppLocalizationsFr([super.locale = 'fr']);
-
-  @override
-  String get newTag => 'nouvelle';
-}
