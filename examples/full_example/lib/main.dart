9dbd554ec2ac78012055a6a10d0345c890c958a9
Majid <mhadaily@gmail.com>
2023-06-06 02:02:18 +0200

    fix generator knobs example

--- examples/full_example/lib/main.dart
+++ examples/full_example/lib/main.dart
@@ -1,25 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:widgetbook/widgetbook.dart';
 
 void main() {
-  runApp(const KnobsExample());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-    required this.title,
-    this.incrementBy = 1,
-    this.countLabel,
-    this.iconData,
-    this.showToolTip = true,
-  }) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
-  final int incrementBy;
-  final String? countLabel;
-  final IconData? iconData;
-  final bool showToolTip;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -30,23 +59,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      _counter += widget.incrementBy;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              widget.countLabel ??
-                  'You have pushed the button this many times:',
+            const Text(
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
@@ -57,87 +117,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: widget.showToolTip ? 'Increment' : null,
-        child: Icon(widget.iconData ?? Icons.add),
-      ),
-    );
-  }
-}
-
-class KnobsExample extends StatelessWidget {
-  const KnobsExample({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Widgetbook.material(
-      addons: [
-        DeviceFrameAddon(
-          devices: [
-            Devices.ios.iPhoneSE,
-            Devices.ios.iPhone12,
-          ],
-        ),
-        TextScaleAddon(
-          scales: [1, 2],
-        ),
-        MaterialThemeAddon(
-          themes: [
-            WidgetbookTheme(
-              data: ThemeData.dark(),
-              name: 'dark',
-            ),
-          ],
-        ),
-      ],
-      directories: [
-        WidgetbookCategory(
-          name: 'Pages',
-          children: [
-            WidgetbookComponent(
-              name: 'On boarding',
-              useCases: [
-                WidgetbookUseCase(
-                  name: 'Home Page',
-                  builder: (context) => MyHomePage(
-                    title: context.knobs
-                        .string(label: 'Title', initialValue: 'Title'),
-                    incrementBy: context.knobs.doubleOrNull
-                            .slider(
-                              label: 'Increment By',
-                              min: 0,
-                              initialValue: 5,
-                              max: 10,
-                              divisions: 10,
-                            )
-                            ?.toInt() ??
-                        0,
-                    countLabel: context.knobs.stringOrNull(
-                      label: 'Count Label',
-                      initialValue: 'Current Count',
-                      description:
-                          'This is the text that appears above the current count of increments',
-                    ),
-                    iconData: context.knobs.list(
-                      label: 'Icon',
-                      options: [
-                        Icons.add,
-                        Icons.crop_square_sharp,
-                        Icons.circle
-                      ],
-                    ),
-                    showToolTip: context.knobs.boolean(
-                      label: 'Show Increment Tool Tip',
-                      description:
-                          'This is the tooltip that is displayed when hovering over the increment button',
-                      initialValue: true,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
