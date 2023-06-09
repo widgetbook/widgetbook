0c42dbc8ef9f2e27f49667f97ac2a51a768fa85d
Youssef Raafat <youssefraafatnasry@gmail.com>
2023-06-09 12:23:57 +0300

    refactor(annotation): remove `@App`'s non-working params (#735)

--- packages/widgetbook_annotation/lib/src/app.dart
+++ packages/widgetbook_annotation/lib/src/app.dart
@@ -1,14 +1,5 @@
 /// Annotates a code element to create the widgetbook main file in the same
 /// folder in which the annotated element is defined.
 class App {
-  const App({
-    this.foldersExpanded = false,
-    this.widgetsExpanded = false,
-  });
-
-  /// Determines folders are expanded by default
-  final bool foldersExpanded;
-
-  /// Determines widgets are expanded by default
-  final bool widgetsExpanded;
+  const App();
 }
