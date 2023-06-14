20689dc389f21d68ea286d478885b05d69f697a8
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-06-14 13:47:53 +0300

    add class doc comment

--- packages/widgetbook/lib/src/fields/fields_composable.dart
+++ packages/widgetbook/lib/src/fields/fields_composable.dart
@@ -2,10 +2,12 @@ import 'package:flutter/widgets.dart';
 
 import 'field.dart';
 
+/// Interface for defining APIs for features that
+/// use [fields] as a building block.
 abstract class FieldsComposable<T> {
   List<Field> get fields;
 
-  /// Converts a query group to a setting of type [T].
+  /// Converts a query group to a value of type [T].
   T valueFromQueryGroup(Map<String, String> group);
 
   /// Converts the [fields] into a [Widget] that will be rendered in the
