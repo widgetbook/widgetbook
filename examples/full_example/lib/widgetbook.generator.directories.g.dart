de491d931f828bb9fc7e6d9d055c8430e783027d
Majid <mhadaily@gmail.com>
2023-06-29 23:41:34 +0200

    rewrite generator

--- examples/full_example/lib/widgetbook.generator.directories.g.dart
+++ examples/full_example/lib/widgetbook.generator.directories.g.dart
@@ -8,24 +8,6 @@
 // AppGenerator
 // **************************************************************************
 
-import 'dart:async';
-import 'dart:core';
-import 'dart:math';
-import 'dart:ui';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/src/material/constants.dart';
-import 'package:flutter/src/material/debug.dart';
-import 'package:flutter/src/material/material_state.dart';
-import 'package:flutter/src/material/slider_theme.dart';
-import 'package:flutter/src/material/theme.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/image.dart';
-import 'package:flutter/widgets.dart';
 import 'package:full_example/components/container.dart';
 import 'package:full_example/components/custom_card.dart';
 import 'package:full_example/components/custom_text_field.dart';
