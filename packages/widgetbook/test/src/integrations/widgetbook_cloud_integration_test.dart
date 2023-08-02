10c016db3d08e07aa9887ba9c560613d58e806e1
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-08-02 16:06:17 +0300

    add integrations tests

---
+++ packages/widgetbook/test/src/integrations/widgetbook_cloud_integration_test.dart
@@ -0,0 +1,96 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:widgetbook/src/knobs/knobs.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+import '../../helper/helper.dart';
+
+class OnNotifyCloudMock extends Mock implements Object {
+  void call(String title, List<Map<String, dynamic>> data);
+}
+
+class MockCloudIntegration extends WidgetbookCloudIntegration {
+  final OnNotifyCloudMock onNotifyMock = OnNotifyCloudMock();
+
+  @override
+  void notifyCloud(
+    String title,
+    List<Map<String, dynamic>> data,
+  ) {
+    onNotifyMock.call(title, data);
+  }
+}
+
+void main() {
+  group(
+    '$WidgetbookCloudIntegration',
+    () {
+      test(
+        'given a state with no addons, '
+        'when onInit is called, '
+        'then addons data is not sent',
+        () {
+          final state = MockWidgetbookState();
+          when(() => state.addons).thenReturn(null);
+
+          final integration = MockCloudIntegration();
+          integration.onInit(state);
+
+          verifyNever(
+            () => integration.onNotifyMock.call(any(), any()),
+          );
+        },
+      );
+
+      test(
+        'when onInit is called, '
+        'then addons data is sent',
+        () {
+          final state = MockWidgetbookState();
+          final addons = [AlignmentAddon()];
+          when(() => state.addons).thenReturn(addons);
+
+          final integration = MockCloudIntegration();
+          integration.onInit(state);
+
+          verify(
+            () => integration.onNotifyMock.call(
+              'addons',
+              addons //
+                  .map((addon) => addon.toJson())
+                  .toList(),
+            ),
+          ).called(1);
+        },
+      );
+
+      test(
+        'when onKnobsRegistered is called, '
+        'then knobs data is sent',
+        () {
+          final state = MockWidgetbookState();
+          final knobs = {
+            'key': StringKnob(
+              label: 'description',
+              value: 'Lorem ipsum',
+            ),
+          };
+
+          when(() => state.knobs).thenReturn(knobs);
+
+          final integration = MockCloudIntegration();
+          integration.onKnobsRegistered(state);
+
+          verify(
+            () => integration.onNotifyMock.call(
+              'knobs',
+              knobs.values //
+                  .map((knob) => knob.toJson())
+                  .toList(),
+            ),
+          ).called(1);
+        },
+      );
+    },
+  );
+}
---
+++ packages/widgetbook/test/src/integrations/widgetbook_integration_test.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+import '../../helper/helper.dart';
+
+class MockIntegration extends Mock implements WidgetbookIntegration {}
+
+void main() {
+  setUpAll(() {
+    registerFallbackValue(MockWidgetbookState());
+  });
+
+  group(
+    '$WidgetbookIntegration',
+    () {
+      testWidgets(
+        'given an integration, '
+        'when Widgetbook is initialized, '
+        'then integration.onInit is called',
+        (tester) async {
+          final integration = MockIntegration();
+          await tester.pumpWidgetWithMaterialApp(
+            Widgetbook.material(
+              integrations: [integration],
+              directories: [],
+            ),
+          );
+
+          verify(
+            () => integration.onInit(any<WidgetbookState>()),
+          ).called(1);
+        },
+      );
+
+      test(
+        'given an integration, '
+        'when state changes, '
+        'then integration.onChange is called',
+        () {
+          final integration = MockIntegration();
+          final state = WidgetbookState(
+            integrations: [integration],
+            directories: [],
+            appBuilder: (_, child) => child,
+          );
+
+          state.notifyListeners();
+
+          verify(
+            () => integration.onChange(state),
+          ).called(1);
+        },
+      );
+
+      test(
+        'given an integration, '
+        'when state notifies knobs ready, '
+        'then integration.onKnobsRegistered is called',
+        () {
+          final integration = MockIntegration();
+          final state = WidgetbookState(
+            integrations: [integration],
+            directories: [],
+            appBuilder: (_, child) => child,
+          );
+
+          state.notifyKnobsReady();
+
+          verify(
+            () => integration.onKnobsRegistered(state),
+          ).called(1);
+        },
+      );
+    },
+  );
+}
