585311a04293d727c50d031a9610991d97926b33
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-08-18 14:26:50 +0300

    refactor: replace `null` device with `NoneDevice`

---
+++ packages/widgetbook/lib/src/addons/device_frame_addon/none_device.dart
@@ -0,0 +1,38 @@
+import 'package:device_frame/device_frame.dart';
+import 'package:flutter/widgets.dart';
+
+class NoneDevice implements DeviceInfo {
+  const NoneDevice._();
+
+  static const NoneDevice instance = NoneDevice._();
+
+  @override
+  String get name => 'None';
+
+  @override
+  DeviceIdentifier get identifier => throw UnimplementedError();
+
+  @override
+  Size get frameSize => throw UnimplementedError();
+
+  @override
+  Size get screenSize => throw UnimplementedError();
+
+  @override
+  double get pixelRatio => throw UnimplementedError();
+
+  @override
+  EdgeInsets? get rotatedSafeAreas => throw UnimplementedError();
+
+  @override
+  EdgeInsets get safeAreas => throw UnimplementedError();
+
+  @override
+  Path get screenPath => throw UnimplementedError();
+
+  @override
+  $DeviceInfoCopyWith<DeviceInfo> get copyWith => throw UnimplementedError();
+
+  @override
+  CustomPainter get framePainter => throw UnimplementedError();
+}
