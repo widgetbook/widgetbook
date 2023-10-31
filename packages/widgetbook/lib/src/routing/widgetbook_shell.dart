4aa112bd55a4544394ba68325c8c8f4a6a5539b8
Mastersam07 <abadasamuelosp@gmail.com>
2023-10-31 23:22:32 +0100

    mobile support

--- packages/widgetbook/lib/src/routing/widgetbook_shell.dart
+++ packages/widgetbook/lib/src/routing/widgetbook_shell.dart
@@ -15,6 +15,21 @@ class WidgetbookShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isMobile = MediaQuery.sizeOf(context).width < 800;
+
+    return isMobile
+        ? MobileWidgetbookShell(child: child)
+        : DesktopWidgetbookShell(child: child);
+  }
+}
+
+class DesktopWidgetbookShell extends StatelessWidget {
+  const DesktopWidgetbookShell({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
     final state = WidgetbookState.of(context);
 
     return ColoredBox(
@@ -59,6 +74,136 @@ class WidgetbookShell extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MobileWidgetbookShell extends StatefulWidget {
+  const MobileWidgetbookShell({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<MobileWidgetbookShell> createState() => _MobileWidgetbookShellState();
+}
+
+class _MobileWidgetbookShellState extends State<MobileWidgetbookShell> {
+  int _pointerCount = 0;
+  bool _isBottomBarVisible = true;
+
+  void _handlePointerDown(PointerDownEvent event) {
+    setState(() {
+      _pointerCount++;
+    });
+  }
+
+  void _handlePointerUp(PointerUpEvent event) {
+    setState(() {
+      _pointerCount--;
+    });
+
+    if (_pointerCount == 0) {
+      if (_pointerCount == 4) {
+        setState(() {
+          _isBottomBarVisible = !_isBottomBarVisible;
+        });
+      }
+    }
+  }
+
+  void _showModalBottomSheet(BuildContext context, Widget child) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (context) {
+        return child;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = WidgetbookState.of(context);
+    return Scaffold(
+      bottomNavigationBar: _isBottomBarVisible
+          ? BottomNavigationBar(
+              items: [
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.navigation),
+                  label: 'Navigation',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.add),
+                  label: 'Addons',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: 'Knobs',
+                ),
+              ],
+              onTap: (index) {
+                switch (index) {
+                  case 0:
+                    _showModalBottomSheet(
+                      context,
+                      NavigationPanel(
+                        initialPath: state.path,
+                        root: state.root,
+                        onNodeSelected: (node) {
+                          WidgetbookState.of(context).updatePath(node.path);
+                        },
+                      ),
+                    );
+                    break;
+                  case 1:
+                    _showModalBottomSheet(
+                      context,
+                      SettingsPanel(
+                        settings: [
+                          if (state.addons != null) ...{
+                            SettingsPanelData(
+                              name: 'Addons',
+                              builder: (context) => WidgetbookState.of(context)
+                                  .effectiveAddons!
+                                  .map((addon) => addon.buildFields(context))
+                                  .toList(),
+                            ),
+                          },
+                        ],
+                      ),
+                    );
+                    break;
+                  case 2:
+                    _showModalBottomSheet(
+                      context,
+                      SettingsPanel(
+                        settings: [
+                          SettingsPanelData(
+                            name: 'Knobs',
+                            builder: (context) => WidgetbookState.of(context)
+                                .knobs
+                                .values
+                                .map((knob) => knob.buildFields(context))
+                                .toList(),
+                          ),
+                        ],
+                      ),
+                    );
+                    break;
+                }
+              },
+            )
+          : null,
+      body: Listener(
+        onPointerDown: _handlePointerDown,
+        onPointerUp: _handlePointerUp,
+        child: Container(
+          // Content of the Widgetbook shell
+          child: widget.child,
+        ),
       ),
     );
   }
