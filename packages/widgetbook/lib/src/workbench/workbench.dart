bfb1edf71c345441fa38643782e6bcf2070c1166
Youssef <youssefraafatnasry@gmail.com>
2023-12-14 11:17:12 +0100

    refactor: wrap workbench with `Scaffold` (#1091)

--- packages/widgetbook/lib/src/workbench/workbench.dart
+++ packages/widgetbook/lib/src/workbench/workbench.dart
@@ -13,44 +13,47 @@ class Workbench extends StatelessWidget {
   Widget build(BuildContext context) {
     final state = WidgetbookState.of(context);
 
-    return SafeBoundaries(
-      child: state.appBuilder(
-        context,
-        ColoredBox(
-          // Background color for the area behind device frame if
-          // the [DeviceFrameAddon] is used.
-          color: Theme.of(context).scaffoldBackgroundColor,
-          child: MultiAddonBuilder(
-            addons: state.addons,
-            builder: (context, addon, child) {
-              final state = WidgetbookState.of(context);
-              final groupMap = FieldCodec.decodeQueryGroup(
-                state.queryParams[addon.groupName],
-              );
+    return Scaffold(
+      // Some addons require a Scaffold to work properly.
+      body: SafeBoundaries(
+        child: state.appBuilder(
+          context,
+          ColoredBox(
+            // Background color for the area behind device frame if
+            // the [DeviceFrameAddon] is used.
+            color: Theme.of(context).scaffoldBackgroundColor,
+            child: MultiAddonBuilder(
+              addons: state.addons,
+              builder: (context, addon, child) {
+                final state = WidgetbookState.of(context);
+                final groupMap = FieldCodec.decodeQueryGroup(
+                  state.queryParams[addon.groupName],
+                );
 
-              final newSetting = addon.valueFromQueryGroup(groupMap);
+                final newSetting = addon.valueFromQueryGroup(groupMap);
 
-              return addon.buildUseCase(
-                context,
-                child,
-                newSetting,
-              );
-            },
-            child: Stack(
-              // The Stack is used to loosen the constraints of
-              // the UseCaseBuilder. Without the Stack, UseCaseBuilder
-              // would expand to the whole size of the Workbench.
-              children: [
-                UseCaseBuilder(
-                  key: ValueKey(state.uri),
-                  builder: (context) {
-                    return WidgetbookState.of(context)
-                            .useCase
-                            ?.build(context) ??
-                        const SizedBox.shrink();
-                  },
-                ),
-              ],
+                return addon.buildUseCase(
+                  context,
+                  child,
+                  newSetting,
+                );
+              },
+              child: Stack(
+                // The Stack is used to loosen the constraints of
+                // the UseCaseBuilder. Without the Stack, UseCaseBuilder
+                // would expand to the whole size of the Workbench.
+                children: [
+                  UseCaseBuilder(
+                    key: ValueKey(state.uri),
+                    builder: (context) {
+                      return WidgetbookState.of(context)
+                              .useCase
+                              ?.build(context) ??
+                          const SizedBox.shrink();
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
