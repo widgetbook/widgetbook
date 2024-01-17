dee1284bf4a4c6fac8e5d45598fc6f5751c508e9
Youssef <youssefraafatnasry@gmail.com>
2024-01-17 14:36:13 +0200

    fix(cli): use staging `appUrl` for non-prod envs (#1129)

--- packages/widgetbook_cli/lib/src/core/environment.dart
+++ packages/widgetbook_cli/lib/src/core/environment.dart
@@ -23,7 +23,7 @@ class StagingEnv extends Environment {
   StagingEnv()
       : super(
           name: 'staging',
-          appUrl: 'https://dev.app.widgetbook.io/',
+          appUrl: 'https://staging.app.widgetbook.io/',
           apiUrl: 'https://staging.api.widgetbook.io/v1/',
         );
 }
@@ -32,7 +32,7 @@ class DebugEnv extends Environment {
   DebugEnv()
       : super(
           name: 'debug',
-          appUrl: 'https://dev.app.widgetbook.io/',
+          appUrl: 'https://staging.app.widgetbook.io/',
           apiUrl: 'http://localhost:3000/v1/',
         );
 }
