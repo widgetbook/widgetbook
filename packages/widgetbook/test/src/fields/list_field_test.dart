f7a9491963f524e732d9bcf9d667613e7accd623
Andrew Brampton <github@bramp.net>
2024-03-15 08:36:12 -0700

    fix: guard `list` knob against null search values (#1152)

--- packages/widgetbook/test/src/fields/list_field_test.dart
+++ packages/widgetbook/test/src/fields/list_field_test.dart
@@ -69,6 +69,26 @@ void main() {
           expect(widget.initialSelection, equals(2));
         },
       );
+
+      testWidgets(
+        'given a state that has a field value, '
+        'when the text is changed to invalid entry "4", '
+        'no exceptions are thrown',
+        (tester) async {
+          await tester.pumpField<int, DropdownMenu<int>>(
+            field,
+            2,
+          );
+
+          await tester.findAndEnter(find.byType(TextField), '4');
+
+          await tester.testTextInput.receiveAction(TextInputAction.done);
+          await tester.pump();
+        },
+        // The test only works on a desktop platforms, as they allow the
+        // drop down text to be edited.
+        variant: TargetPlatformVariant.only(TargetPlatform.macOS),
+      );
     },
   );
 }
