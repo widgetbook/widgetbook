994486269555346c97eaaa254a3a8580ea506da4
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2024-06-10 13:44:06 +0200

    use backwards compatible approach

--- packages/widgetbook/lib/src/themes.dart
+++ packages/widgetbook/lib/src/themes.dart
@@ -22,7 +22,6 @@ class Themes {
     onErrorContainer: Color(0xFFFFDAD6),
     surface: Color(0xFF1A1C1E),
     onSurface: Color(0xFFE3E2E6),
-    surfaceContainerHighest: Color(0xFF43474E),
     onSurfaceVariant: Color(0xFFC3C6CF),
     outline: Color(0xFF8D9199),
     onInverseSurface: Color(0xFF1A1C1E),
@@ -30,6 +29,17 @@ class Themes {
     inversePrimary: Color(0xFF0060A7),
     shadow: Color(0xFF000000),
     surfaceTint: Color(0xFFA1C9FF),
+
+    // The following parameters are deprecated in Flutter 3.22.0,
+    // But we cannot remove them because our minimum version is 3.7.0,
+    // and these parameters are required there.
+
+    // ignore: deprecated_member_use
+    background: Color(0xFF1A1C1E),
+    // ignore: deprecated_member_use
+    onBackground: Color(0xFFE3E2E6),
+    // ignore: deprecated_member_use
+    surfaceVariant: Color(0xFF43474E),
   );
 
   static const _lightColorScheme = ColorScheme(
@@ -51,7 +61,6 @@ class Themes {
     errorContainer: Color(0xFFFFDAD6),
     onErrorContainer: Color(0xFF410002),
     surface: Color(0xFFFDFCFF),
-    surfaceContainerHighest: Color(0xFFDFE2EB),
     onSurface: Color(0xFF1A1C1E),
     onSurfaceVariant: Color(0xFF43474E),
     outline: Color(0xFF73777F),
@@ -60,22 +69,42 @@ class Themes {
     inversePrimary: Color(0xFFA1C9FF),
     shadow: Color(0xFF000000),
     surfaceTint: Color(0xFF0060A7),
+
+    // The following parameters are deprecated in Flutter 3.22.0,
+    // But we cannot remove them because our minimum version is 3.7.0,
+    // and these parameters are required there.
+
+    // ignore: deprecated_member_use
+    background: Color(0xFFFDFCFF),
+    // ignore: deprecated_member_use
+    onBackground: Color(0xFF1A1C1E),
+    // ignore: deprecated_member_use
+    surfaceVariant: Color(0xFFDFE2EB),
   );
 
-  static final _inputDecorationTheme = InputDecorationTheme(
-    filled: true,
-    isDense: true,
-    focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(
-        color: _darkColorScheme.primary,
+  static InputDecorationTheme _buildInputTheme(
+    ColorScheme colorScheme,
+  ) {
+    return InputDecorationTheme(
+      filled: true,
+      isDense: true,
+      // By default, this is [ColorScheme.surfaceContainerHighest], but we
+      // need to override it to [ColorScheme.surfaceVariant] due to the minimum
+      // Flutter version being 3.7.0, which does not have the new parameter.
+      // ignore: deprecated_member_use
+      fillColor: colorScheme.surfaceVariant,
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: colorScheme.primary,
+        ),
       ),
-    ),
-    enabledBorder: const OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.transparent,
+      enabledBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.transparent,
+        ),
       ),
-    ),
-  );
+    );
+  }
 
   static ThemeData dark = ThemeData(
     useMaterial3: true,
@@ -86,9 +115,9 @@ class Themes {
     sliderTheme: SliderThemeData(
       overlayShape: SliderComponentShape.noThumb,
     ),
-    inputDecorationTheme: _inputDecorationTheme,
+    inputDecorationTheme: _buildInputTheme(_darkColorScheme),
     dropdownMenuTheme: DropdownMenuThemeData(
-      inputDecorationTheme: _inputDecorationTheme,
+      inputDecorationTheme: _buildInputTheme(_darkColorScheme),
     ),
     expansionTileTheme: const ExpansionTileThemeData(
       collapsedShape: RoundedRectangleBorder(),
@@ -105,9 +134,9 @@ class Themes {
     sliderTheme: SliderThemeData(
       overlayShape: SliderComponentShape.noThumb,
     ),
-    inputDecorationTheme: _inputDecorationTheme,
+    inputDecorationTheme: _buildInputTheme(_lightColorScheme),
     dropdownMenuTheme: DropdownMenuThemeData(
-      inputDecorationTheme: _inputDecorationTheme,
+      inputDecorationTheme: _buildInputTheme(_lightColorScheme),
     ),
     expansionTileTheme: const ExpansionTileThemeData(
       collapsedShape: RoundedRectangleBorder(),
