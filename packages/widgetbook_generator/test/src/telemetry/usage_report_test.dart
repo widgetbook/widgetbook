0c7940dd3d50cb702e9e7cb4b61dd8c2930267c3
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2024-06-11 10:05:17 +0200

    fix tests

--- packages/widgetbook_generator/test/src/telemetry/usage_report_test.dart
+++ packages/widgetbook_generator/test/src/telemetry/usage_report_test.dart
@@ -8,6 +8,7 @@ void main() {
     final report = UsageReport.from(
       version: '3.x.x',
       project: 'test',
+      projectId: 'yyyy',
       trackingId: 'xxxx',
       useCases: [
         MockUseCaseMetadata(
@@ -94,6 +95,7 @@ void main() {
             '\$insert_id': report.id,
             'version': '3.x.x',
             'project': report.project,
+            'project_id': report.projectId,
             'packages': report.packages.toList(),
             'components': 2,
             'use_cases': 5,
