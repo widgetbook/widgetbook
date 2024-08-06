9af7323753f0ff78d5c3d2c4affbef699117d13a
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2024-08-06 12:38:34 +0200

    fix typos

--- packages/widgetbook/test/src/routing/non_ascii_chars_test.dart
+++ packages/widgetbook/test/src/routing/non_ascii_chars_test.dart
@@ -10,8 +10,8 @@ void main() {
     '$AppRouteConfig for non-ascii characters',
     () {
       testWidgets(
-        'given query parameter in URL address '
-        'when query contains encoded special characters like: ` `, `,` etc. '
+        'given query parameter in URL address, '
+        'when query contains encoded special characters like: ` `, `,` etc., '
         'then this query string should decode',
         (tester) async {
           final state = await tester.pumpWidgetWithQueryParams(
@@ -34,7 +34,7 @@ void main() {
       );
 
       testWidgets(
-        'given non-ASCII characters in URL'
+        'given non-ASCII characters in URL, '
         'then the application is open without decoding',
         (tester) async {
           const textKey = Key('knob-string-field');
@@ -46,17 +46,23 @@ void main() {
               final text = context.knobs.string(
                 label: keyOfData,
               );
-              return Text('$text', key: textKey);
+              return Text(
+                '$text',
+                key: textKey,
+              );
             },
           );
+
           await tester.pumpAndSettle();
 
           // Checking not initialized field
-          final emptyTextWidget =
-              await tester.widget<Text>(find.byKey(textKey));
+          final emptyTextWidget = await tester.widget<Text>(
+            find.byKey(textKey),
+          );
+
           expect(emptyTextWidget.data, '');
 
-          // Simulate as opened URL via broweser.
+          // Simulate as opened URL via browser.
           state.updateFromRouteConfig(
             AppRouteConfig(
               uri: state.uri.replace(
@@ -69,15 +75,17 @@ void main() {
 
           await tester.pumpAndSettle();
 
-          final actualTextWidget =
-              await tester.widget<Text>(find.byKey(textKey));
+          final actualTextWidget = await tester.widget<Text>(
+            find.byKey(textKey),
+          );
+
           expect(actualTextWidget.data, valueOfData);
         },
       );
 
       testWidgets(
-        'given non-ASCII characters and special charater (`,`) in URL'
-        'then captured error bacause URL\'s characters are invalid',
+        'given non-ASCII characters and special charter (`,`) in URL, '
+        'then captured error because URL\'s characters are invalid',
         (tester) async {
           final binding = TestWidgetsFlutterBinding.ensureInitialized();
           const textKey = Key('knob-string-field');
@@ -91,16 +99,21 @@ void main() {
               final text = context.knobs.string(
                 label: keyOfData,
               );
-              return Text('$text', key: textKey);
+              return Text(
+                '$text',
+                key: textKey,
+              );
             },
           );
           await tester.pumpAndSettle();
+
           // Checking for not initialized field
-          final emptyTextWidget =
-              await tester.widget<Text>(find.byKey(textKey));
+          final emptyTextWidget = await tester.widget<Text>(
+            find.byKey(textKey),
+          );
           expect(emptyTextWidget.data, '');
 
-          // Simulate as opened URL via broweser.
+          // Simulate as opened URL via browser
           state.updateFromRouteConfig(
             AppRouteConfig(
               uri: state.uri.replace(
@@ -110,6 +123,7 @@ void main() {
               ),
             ),
           );
+
           await tester.pumpAndSettle();
           final error = binding.takeException();
           expect(error, isRangeError);
