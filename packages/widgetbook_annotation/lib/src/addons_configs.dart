8e99fcf76dac52dc1548ed7f41bc5041ea7df539
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2024-08-22 11:07:06 +0300

    improve api docs

--- packages/widgetbook_annotation/lib/src/addons_configs.dart
+++ packages/widgetbook_annotation/lib/src/addons_configs.dart
@@ -1,5 +1,30 @@
 import 'addon_config.dart';
 
+/// {@template cloud_addons_configs}
+/// A map between the name of the configuration and the list of addon
+/// configurations. This allows to create a matrix of addon configurations
+/// to test on Widgetbook Cloud while taking snapshots.
+///
+/// The key should be unique and descriptive name for the configuration.
+/// For example, if this configuration tests the dark mode and
+/// the German locale, then the name can be "Dark DE".
+///
+/// The value should be a list of [AddonConfig]s.
+/// For example:
+///
+/// ```dart
+/// {
+///   'Dark DE': [
+///     ThemeAddonConfig('Dark'),
+///     LocalizationAddonConfig('de'),
+///     AddonConfig('AddonName', 'name:value,otherName:otherValue'),
+///   ],
+/// }
+/// ```
+///
+/// See [AddonConfig] for more information about other
+/// available configurations.
+/// {@endtemplate}
 typedef AddonsConfigs = Map<String, Iterable<AddonConfig>>;
 
 extension AddonsConfigsExtension on AddonsConfigs {
--- packages/widgetbook_annotation/lib/src/app.dart
+++ packages/widgetbook_annotation/lib/src/app.dart
@@ -1,4 +1,3 @@
-import 'addon_config.dart';
 import 'addons_configs.dart';
 
 /// Annotates a code element to create the widgetbook main file in the same
@@ -8,28 +7,6 @@ class App {
     this.cloudAddonsConfigs,
   });
 
-  /// A map between the name of the configuration and the list of addon
-  /// configurations. This allows to create a matrix of addon configurations
-  /// to test on Widgetbook Cloud while taking snapshots.
-  ///
-  /// The key should be unique and descriptive name for the configuration.
-  /// For example, if this configuration tests the dark mode and
-  /// the German locale, then the name can be "Dark DE".
-  ///
-  /// The value should be a list of [AddonConfig]s.
-  /// For example:
-  ///
-  /// ```dart
-  /// {
-  ///   'Dark DE': [
-  ///     ThemeAddonConfig('Dark'),
-  ///     LocalizationAddonConfig('de'),
-  ///     AddonConfig('AddonName', 'name:value,otherName:otherValue'),
-  ///   ],
-  /// }
-  /// ```
-  ///
-  /// See [AddonConfig] for more information about other
-  /// available configurations.
+  /// {@macro cloud_addons_configs}
   final AddonsConfigs? cloudAddonsConfigs;
 }
