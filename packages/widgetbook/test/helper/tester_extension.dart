d7edf12ebfbd1b3e41998cf5206444236516718d
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2024-12-11 16:55:48 +0200

    fix: expose `WidgetbookScope`

--- packages/widgetbook/test/helper/tester_extension.dart
+++ packages/widgetbook/test/helper/tester_extension.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:widgetbook/src/state/widgetbook_scope.dart';
 import 'package:widgetbook/src/themes.dart';
 import 'package:widgetbook/widgetbook.dart';
 
