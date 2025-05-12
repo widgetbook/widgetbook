f9114922653da9907b5cd2c6baa6320c11df4501
Jens Horstmann <jens@widgetbook.io>
2025-05-12 19:38:54 +0200

    docs: improve knobs (#1436)

---
+++ examples/feature_preview/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'main.directories.g.dart';
+
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      directories: directories,
+      addons: [
+        MaterialThemeAddon(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: ThemeData.light()),
+            WidgetbookTheme(name: 'Dark', data: ThemeData.dark()),
+          ],
+        ),
+        AlignmentAddon(),
+        ViewportAddon([Viewports.none, IosViewports.iPhone13]),
+      ],
+    );
+  }
+}
