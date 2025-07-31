cb5ba6ec25f72e609f33b4bd8bed7e841028d6a9
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2025-07-31 15:47:14 +0200

    document workbench

--- packages/widgetbook/lib/src/workbench/safe_boundaries.dart
+++ packages/widgetbook/lib/src/workbench/safe_boundaries.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 /// Sets safe boundaries around [child] for proper rendering.
 /// This is needed for widgets that depend on [MediaQuery],
 /// for example: widgets from `flutter_screenutil` package.
+@internal
 class SafeBoundaries extends StatelessWidget {
   const SafeBoundaries({
     super.key,
