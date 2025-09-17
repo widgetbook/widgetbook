94f4e3080d1e484b83c0a2fca2d9ad29e4615a20
yousinix <youssefraafatnasry@gmail.com>
2025-09-17 12:39:08 +0200

    refactor: bound meta type param (#1595)

--- packages/widgetbook/lib/src/core/meta.dart
+++ packages/widgetbook/lib/src/core/meta.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/widgets.dart';
+
 import 'story_args.dart';
 
-class Meta<T> {
+class Meta<TWidget extends Widget> {
   const Meta({
     String? name,
     String? path,
@@ -16,11 +18,11 @@ class Meta<T> {
   /// Creates a copy of this using the provided [path] for late initialization.
   /// If [$name] was already set, it should have precedence over [name].
   /// If [$path] was already set, it should have precedence over [path].
-  Meta<T> init({
+  Meta<TWidget> init({
     required String name,
     required String path,
   }) {
-    return Meta<T>(
+    return Meta<TWidget>(
       name: $name ?? name,
       path: $path ?? path,
     );
@@ -28,7 +30,7 @@ class Meta<T> {
 }
 
 /// Same as [Meta] but for custom [StoryArgs].
-class MetaWithArgs<TWidget, TArgs> extends Meta<TWidget> {
+class MetaWithArgs<TWidget extends Widget, TArgs> extends Meta<TWidget> {
   const MetaWithArgs({
     super.name,
     super.path,
