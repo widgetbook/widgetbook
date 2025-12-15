98edf5fbe323e005820ea81af80139d1c7abac8c
yousinix <youssefraafatnasry@gmail.com>
2025-12-15 22:45:02 +0100

    feat(vsc): add initial extension (#1781)
    A minimal Visual Studio Code extension that helps navigating between
    widgets and their respective stories file.
    
    https://github.com/user-attachments/assets/e24931be-04bb-4a5d-87c5-177d63a8b995

---
+++ extensions/vsc/README.md
@@ -0,0 +1,5 @@
+# Widgetbook VSC Extension
+
+## Features
+
+- View Component CodeLens: Navigate easily between your widget (in you app) and its corresponding component (in Widgetbook).
---
+++ extensions/vsc/example/widgetbook/lib/my_widget.stories.dart
@@ -0,0 +1,9 @@
+import 'package:example/my_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+part 'my_widget.stories.book.dart';
+
+const meta = Meta<MyWidget>();
+
+final $Default = _Story();
