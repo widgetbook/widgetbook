89ec972212c79bb0f6f98f6b73a222c108693376
Jens Horstmann <jens@widgetbook.io>
2026-02-10 16:34:14 +0100

    chore(cli): release v3.12.0 (#1832)

--- packages/widgetbook_cli/lib/metadata.dart
+++ packages/widgetbook_cli/lib/metadata.dart
@@ -8,4 +8,4 @@ const cliDescription = 'Widgetbook CLI';
 const packageName = 'widgetbook_cli';
 
 /// The version as in pubspec.yaml
-const packageVersion = '3.11.1';
+const packageVersion = '3.12.0';
