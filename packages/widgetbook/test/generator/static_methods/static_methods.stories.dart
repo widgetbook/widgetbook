70f68d77b06e2d861690f297c756e76b0a940e3c
Jens Horstmann <jens@widgetbook.io>
2026-03-04 10:47:15 +0100

    fix: update formatting

--- packages/widgetbook/test/generator/static_methods/static_methods.stories.dart
+++ packages/widgetbook/test/generator/static_methods/static_methods.stories.dart
@@ -6,16 +6,16 @@ part 'static_methods.stories.g.dart';
 typedef MyBuilder = Widget Function(BuildContext context);
 
 class StaticMethodWidget extends StatelessWidget {
-  static Widget _defaultBuilder(BuildContext context) {
-    return const Text('default');
-  }
-
   const StaticMethodWidget({
     super.key,
     this.builder = _defaultBuilder,
   });
 
   final MyBuilder builder;
+
+  static Widget _defaultBuilder(BuildContext context) {
+    return const Text('default');
+  }
 
   @override
   Widget build(BuildContext context) => builder(context);
