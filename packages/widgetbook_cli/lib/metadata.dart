e3c1a150ff751e1386ceb1555d8a193e13184422
Jens Horstmann <jens@widgetbook.io>
2026-08-01 10:31:50 +0200

    chore(cli): release v4.0.0-beta.11

--- packages/widgetbook_cli/lib/metadata.dart
+++ packages/widgetbook_cli/lib/metadata.dart
@@ -8,7 +8,7 @@ const cliDescription = 'Widgetbook CLI';
 const packageName = 'widgetbook_cli';
 
 /// The version as in pubspec.yaml
-const packageVersion = '4.0.0-beta.10';
+const packageVersion = '4.0.0-beta.11';
 
 /// The widgetbook package version to install via `widgetbook init`
-const widgetbookVersion = '4.0.0-beta.10';
+const widgetbookVersion = '4.0.0-beta.11';
